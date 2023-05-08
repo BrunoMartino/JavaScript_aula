cc36e8d262d964d4678489a06de00f5c175dba5f
Bruno Galvao Martins <bruno-g-martins@outlook.com>
2023-05-08 20:03:42 -0300

    accordion refatorado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import ScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/animacao-scroll.js";
-import initAccordion from "./modules/accordion.js";
+import Accordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
@@ -14,7 +14,9 @@ import initNumerosAnimal from "./modules/numerosAnimal.js";
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
-initAccordion();
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 initTabNav();
 initModal();
 initTooltip();

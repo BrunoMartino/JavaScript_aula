55a8d22ed710df1b87a656dd873a984dfe023082
Bruno Galvao Martins <bruno-g-martins@outlook.com>
2023-05-08 21:38:56 -0300

    tooltip refatorada

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import initAnimacaoScroll from "./modules/animacao-scroll.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
-import initTooltip from "./modules/tooltip.js";
+import Tooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu_mobile.js";
 import initDate from "./modules/date_object.js";
@@ -30,7 +30,9 @@ const modal = new Modal(
 );
 modal.init();
 
-initTooltip();
+const tooltip = new Tooltip("[data-tooltip]");
+tooltip.init();
+
 initDropdownMenu();
 initMenuMobile();
 initDate();

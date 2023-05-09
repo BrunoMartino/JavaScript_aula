9640f28681f2ed55bab0d9f4fe6a83a5500641a7
Bruno Galvao Martins <bruno-g-martins@outlook.com>
2023-05-09 20:59:17 -0300

    fetch bitcoin refatorado

--- js/modules/fetchBitcoin.js
+++ js/modules/fetchBitcoin.js
@@ -1,13 +1,13 @@
-export default function initFetchBitcoin() {}
+export default function fetchBitcoin(url, target) {
+  fetch(url)
+    .then((r) => r.json())
+    .then((json) => {
+      const btcPreco = document.querySelector(target);
+      const doarPreco = (1000 / json.BRL.sell).toFixed(4);
+      btcPreco.innerText = "BTC " + doarPreco.replace(".", ",");
+    })
+    .catch((erro) => {
+      console.log(Error(erro));
+    });
+}
 //
-
-fetch("https://blockchain.info/ticker")
-  .then((r) => r.json())
-  .then((json) => {
-    const btcPreco = document.querySelector(".btc-preco");
-    const doarPreco = (1000 / json.BRL.sell).toFixed(4);
-    btcPreco.innerText = "BTC " + doarPreco.replace(".", ",");
-  })
-  .catch((erro) => {
-    console.log(Error(erro));
-  });

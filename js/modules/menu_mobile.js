47fd5f16704871d0d78199cdc1262b7301b20eaa
Bruno Galvao Martins <bruno-g-martins@outlook.com>
2023-05-09 13:25:40 -0300

    teste botao menu mobile 2

--- js/modules/menu_mobile.js
+++ js/modules/menu_mobile.js
@@ -3,7 +3,7 @@ import outsideClick from "./outsideclick.js";
 export default function initMenuMobile() {
   const menuButton = document.querySelector('[data-menu="button"]');
   const menuList = document.querySelector('[data-menu="list"]');
-  const eventos = ["touchstart"];
+  const eventos = ["click"];
   if (menuButton) {
     function openMenu(event) {
       menuList.classList.add("ativo");

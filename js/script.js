505ae151977e8197db2d23dd991ff072f0bc1dd4
Bruno Galvao Martins <bruno-g-martins@outlook.com>
2023-05-12 23:29:15 -0300

    corrigindo caminhos

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ dropdownMenu.init();
 const funcionamento = new Funcionamento("[data-semana]");
 funcionamento.init();
 
-fetchAnimais("../animaisapi.json", ".numeros-grid");
+fetchAnimais("././animaisapi.json", ".numeros-grid");
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
 
 const slide = new SlideNav(".slide", ".wrapper");
